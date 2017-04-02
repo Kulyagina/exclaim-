6b336b4a37dc4ae9bafcc81fc2a73c66ecab2179
Тася Кулягина < >
2017-04-02 14:17:07 +0300

    commit n3

--- scripts/fixed-menu.js
+++ scripts/fixed-menu.js
@@ -4,9 +4,11 @@ var elH = el.getBoundingClientRect().top;
 var  m = document.querySelector(".goods");
 document.addEventListener('scroll', function (e) {
     var scrollTop = window.pageYOffset;
-	if(scrollTop >= elH)
-	        el.classList.add('scrollTop');
-    else
-            el.classList.remove('scrollTop');
+	if(scrollTop >= elH) {
+        el.classList.add('scrollTop');
+		    }
+    else {
+        el.classList.remove('scrollTop');
+    }
     m.style.paddingTop = elHeight  + "px";
 });
