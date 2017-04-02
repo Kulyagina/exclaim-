8e95b71a8252eef02f501516e32246dd2c1db68e
Тася Кулягина < >
2017-04-02 13:17:46 +0300

    change fixed-menu

--- scripts/fixed-menu.js
+++ scripts/fixed-menu.js
@@ -1,19 +1,15 @@
 var el = document.querySelector('.menu');
 var elHeight = el.offsetHeight;
-var elH = document.querySelector('header').clientHeight;
+var elH = el.getBoundingClientRect().top;
+var  m = document.querySelector(".goods");
 document.addEventListener('scroll', function (e) {
-	var  m = document.querySelector(".goods");
-
 	var scrollTop = window.pageYOffset;
-
 	if(scrollTop >= elH) {
-
         el.classList.add('scrollTop');
-        m.style.paddingTop = (elHeight + el.style.Height ) + "px";
-				console.log(m.style.paddingTop);
+        m.style.paddingTop = (elHeight + el.offsetHeight  ) + "px";
     }
     else {
-        el.classList.remove('scrollTop');
-        m.style.paddingTop = el.style.Height + "px";
-    }
-});
+            el.classList.remove('scrollTop');
+            m.style.paddingTop = el.offsetHeight  + "px";
+        }
+    });
