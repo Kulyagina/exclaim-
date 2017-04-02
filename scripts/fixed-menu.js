fc27fc7e8699065c18190ca0d190fce0648b02a7
Тася Кулягина < >
2017-04-02 13:40:07 +0300

    second change

--- scripts/fixed-menu.js
+++ scripts/fixed-menu.js
@@ -3,13 +3,10 @@ var elHeight = el.offsetHeight;
 var elH = el.getBoundingClientRect().top;
 var  m = document.querySelector(".goods");
 document.addEventListener('scroll', function (e) {
-	var scrollTop = window.pageYOffset;
-	if(scrollTop >= elH) {
-        el.classList.add('scrollTop');
-        m.style.paddingTop = (elHeight + el.offsetHeight  ) + "px";
-    }
-    else {
+    var scrollTop = window.pageYOffset;
+	if(scrollTop >= elH)
+	        el.classList.add('scrollTop');
+    else
             el.classList.remove('scrollTop');
-            m.style.paddingTop = el.offsetHeight  + "px";
-        }
-    });
+    m.style.paddingTop = elHeight  + "px";
+});
